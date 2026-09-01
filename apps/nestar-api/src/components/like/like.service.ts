ad67f8e20581683220b3da3ea647e946beab6872
Jonny <jonik0744clash@email.com>
2026-09-01 19:09:05 +0900

    feat: develop likeTargetMember GraphQL APIs

--- apps/nestar-api/src/components/like/like.service.ts
+++ apps/nestar-api/src/components/like/like.service.ts
@@ -1,17 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { MemberService } from '../member/member.service';
-import { PropertyService } from '../property/property.service';
-import { BoardArticleService } from '../board-article/board-article.service';
+// import { MemberService } from '../member/member.service';
+// import { PropertyService } from '../property/property.service';
+// import { BoardArticleService } from '../board-article/board-article.service';
 import { Like } from '../../libs/dto/like/like';
+import { LikeInput } from '../../libs/dto/like/like.input';
+import { T } from '../../libs/types/common';
+import { Message } from '../../libs/enums/common.enum';
 
 @Injectable()
 export class LikeService {
 	constructor(
 		@InjectModel('Like') private readonly likeModel: Model<Like>,
-		private readonly memberService: MemberService,
-		private readonly propertyService: PropertyService,
-		private readonly boardArticleService: BoardArticleService,
+		// private readonly memberService: MemberService,
+		// private readonly propertyService: PropertyService,
+		// private readonly boardArticleService: BoardArticleService,
 	) {}
+
+	public async toggleLike(input: LikeInput): Promise<number> {
+		const search: T = { memberId: input.memberId, likeRefId: input.likeRefId };
+		const exist = await this.likeModel.findOne(search).exec();
+		let modifier = 1;
+
+		if (exist) {
+			await this.likeModel.findOneAndDelete(search).exec();
+			modifier = -1;
+		} else {
+			try {
+				await this.likeModel.create(input);
+			} catch (err) {
+				const message = err instanceof Error ? err.message : 'Unknown error occurred';
+				console.log('Error: Service.model', message);
+				throw new BadRequestException(Message.CREATE_FAILED);
+			}
+		}
+
+		console.log(`- Like modifier ${modifier} -`);
+		return modifier;
+	}
 }
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -11,6 +11,9 @@ import { ViewService } from '../view/view.service';
 import { ViewGroup } from '../../libs/enums/view.enum';
 import { T } from '../../libs/types/common';
 import type { ObjectId, StatisticModifier } from '../../libs/types/common';
+import { LikeInput } from '../../libs/dto/like/like.input';
+import { LikeGroup } from '../../libs/enums/like.enum';
+import { LikeService } from '../like/like.service';
 
 @Injectable()
 export class MemberService {
@@ -18,6 +21,7 @@ export class MemberService {
 		@InjectModel('Member') private readonly memberModel: Model<Member>,
 		private authService: AuthService,
 		private viewService: ViewService,
+		private likeService: LikeService,
 	) {}
 
 	public async signup(input: MemberInput): Promise<Member> {
@@ -127,6 +131,24 @@ export class MemberService {
 			.exec();
 		if (!result) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
 		return result[0] as Members;
+	}
+
+	public async likeTargetMember(memberId: ObjectId, likeRefId: ObjectId): Promise<Member> {
+		const target = await this.memberModel.findOne({ _id: likeRefId, memberStatus: MemberStatus.ACTIVE }).exec();
+		if (!target) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+		const input: LikeInput = {
+			memberId: memberId,
+			likeRefId: likeRefId,
+			likeGroup: LikeGroup.MEMBER,
+		};
+
+		//LIKE TOGGLE
+		const modifier: number = await this.likeService.toggleLike(input);
+		const result = await this.memberStatsEditor({ _id: likeRefId, targetKey: 'memberLikes', modifier: modifier });
+
+		if (!result) throw new InternalServerErrorException(Message.SOMETHING_WENT_WRONG);
+		return result;
 	}
 
 	public async getAllMembersByAdmin(input: MembersInquiry): Promise<Members> {
